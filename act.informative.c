aa9814a68b24228b91d1d36068454ad79f8919fa
BuildTools <unconfigured@null.spigotmc.org>
2017-12-12 14:55:03 -0600

    test

--- act.informative.c
+++ act.informative.c
@@ -1374,7 +1374,8 @@ char *reduct_desc(struct damreduct_type *reduct) {
 }
 
 ACMD(do_skills) {
-	switch (*argument) {
+	char *arg;
+	switch (LOWER(*arg)) {
 		case 'weapon':
 			send_to_char(ch, "weapon skills\r\n");
 			break;
