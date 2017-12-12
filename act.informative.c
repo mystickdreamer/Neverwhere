512eca7fc6c758ac56d450dd766fc56e78162a2f
BuildTools <unconfigured@null.spigotmc.org>
2017-12-12 17:13:33 -0600

    test

--- act.informative.c
+++ act.informative.c
@@ -1390,14 +1390,14 @@ ACMD(do_skills) {
 	if (*name = "weapon") {
 		//if (*arg == "weapon") {
 		send_to_char(ch, "@r=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=@BWeapons@r-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=@n\r\n");
-		send_to_char(ch, "@r=-  @cSmall Edged@n:@Y%5d  @cSmall Blunt@n:@Y%5d @cShortBow@n:@Y%5d @cLight Crossbow@n:@Y%5d @cLight Thrown@n:@Y%5d\r\n",
+		send_to_char(ch, "@r=-  @cSmall Edged@n:@Y%5d  @cSmall Blunt@n:@Y%5d @cShortBow@n:@Y%5d @cLight Crossbow@n:@Y%5d @cLight Thrown@n:@Y%5d -=\r\n",
 			GET_SKILL_BASE(ch, SKILL_WEAPON_SMALL_EDGED), GET_SKILL_BASE(ch, SKILL_WEAPON_SMALL_BLUNT),
 			GET_SKILL_BASE(ch, SKILL_WEAPON_SHORTBOW), GET_SKILL_BASE(ch, SKILL_WEAPON_LIGHT_CROSSBOW), GET_SKILL_BASE(ch, SKILL_WEAPON_LIGHT_THROWN));
-		send_to_char(ch, "@r=- @cMedium Edged@n:@Y%5d @cMedium Blunt@n:@Y%5d  @cLongBow@n:@Y%5d @cHeavy Crossbow@n:@Y%5d @cHeavy Thrown@n:@Y%5d\r\n", 
+		send_to_char(ch, "@r=- @cMedium Edged@n:@Y%5d @cMedium Blunt@n:@Y%5d  @cLongBow@n:@Y%5d @cHeavy Crossbow@n:@Y%5d @cHeavy Thrown@n:@Y%5d -=\r\n", 
 			GET_SKILL_BASE(ch, SKILL_WEAPON_MEDIUM_EDGED), GET_SKILL_BASE(ch, SKILL_WEAPON_MEDIUM_BLUNT), 
 			GET_SKILL_BASE(ch, SKILL_WEAPON_LONGBOW), GET_SKILL_BASE(ch, SKILL_WEAPON_HEAVY_CROSSBOW), GET_SKILL_BASE(ch, SKILL_WEAPON_LIGHT_CROSSBOW), 
 			GET_SKILL_BASE(ch, SKILL_WEAPON_HEAVY_THROWN));
-		send_to_char(ch, "@r=-  @cHeavy Edged@n:@Y%5d  @cHeavy Blunt@n:@Y%5d  @cPolearm@n:@Y%5d       @cBackstab@n:@Y%5d\r\n", 
+		send_to_char(ch, "@r=-  @cHeavy Edged@n:@Y%5d  @cHeavy Blunt@n:@Y%5d  @cPolearm@n:@Y%5d       @cBackstab@n:@Y%5d                   -=\r\n", 
 			GET_SKILL_BASE(ch, SKILL_WEAPON_HEAVY_EDGED), GET_SKILL_BASE(ch, SKILL_WEAPON_HEAVY_BLUNT), 
 			GET_SKILL_BASE(ch, SKILL_WEAPON_POLEARM), GET_SKILL_BASE(ch, SKILL_WEAPON_BACKSTAB));
 				send_to_char(ch, "@r=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-@n\r\n");
