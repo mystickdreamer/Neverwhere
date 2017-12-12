eaddbd553c50dd188b7a6cc2a920ac8fe45718d1
BuildTools <unconfigured@null.spigotmc.org>
2017-12-12 16:45:26 -0600

    test

--- act.informative.c
+++ act.informative.c
@@ -1390,12 +1390,12 @@ ACMD(do_skills) {
 	if (*name = "weapon") {
 		//if (*arg == "weapon") {
 		send_to_char(ch, "@r=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=@BWeapons@r-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=@n\r\n");
-		send_to_char(ch, "@r=-  @cSmall Edged@n:@Y%d   @cMedium Edged@n:@Y%d   @cHeavy Edged@n:@Y%d\r\n",
-			GET_SKILL_BASE(ch, SKILL_WEAPON_SMALL_EDGED), GET_SKILL_BASE(ch, SKILL_WEAPON_MEDIUM_EDGED),
+		send_to_char(ch, "@r=-  @cSmall Edged@n:@Y%d   @cSmall Blunt@n:@Y%d   @cHeavy Edged@n:@Y%d\r\n",
+			GET_SKILL_BASE(ch, SKILL_WEAPON_SMALL_EDGED), GET_SKILL_BASE(ch, SKILL_WEAPON_SMALL_BLUNT),
 			GET_SKILL_BASE(ch, SKILL_WEAPON_HEAVY_EDGED));
 		send_to_char(ch, "@r=- @cMedium Edged@n:@Y%d\r\n", 
 			GET_SKILL_BASE(ch, SKILL_WEAPON_MEDIUM_EDGED));
-		send_to_char(ch, "@r=- @cHeavy Edged@n:@Y%d\r\n", 
+		send_to_char(ch, "@r=-  @cHeavy Edged@n:@Y%d\r\n", 
 			GET_SKILL_BASE(ch, SKILL_WEAPON_HEAVY_EDGED));
 
 	}
