891f39cd9da86a6edda163245e4fbe281b2c5ded
BuildTools <unconfigured@null.spigotmc.org>
2017-12-12 18:53:19 -0600

    do_skills

--- act.informative.c
+++ act.informative.c
@@ -1458,7 +1458,7 @@ ACMD(do_skills) {
 		//	if (*name == "weapon") {
 		
 		send_to_char(ch, "@r=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=@BCraft@r-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=@n\r\n");
-		send_to_char(ch, "@r=-  @cMining@n:@Y%5d   @cForage@n:@Y%5d   @cForestry@n:@Y%5d   @cCooking@n:@Y%5d     @cBlacksmithing@n:@Y%5d     @r-=\r\n",
+		send_to_char(ch, "@r=-  @cMining@n:@Y%5d   @cForage@n:@Y%5d   @cForestry@n:@Y%5d     @cCooking@n:@Y%5d     @cBlacksmithing@n:@Y%5d     @r-=\r\n",
 			GET_SKILL_BASE(ch, SKILL_WEAPON_SMALL_EDGED), GET_SKILL_BASE(ch, SKILL_WEAPON_SMALL_BLUNT),
 			GET_SKILL_BASE(ch, SKILL_WEAPON_SHORTBOW), GET_SKILL_BASE(ch, SKILL_WEAPON_LIGHT_CROSSBOW), GET_SKILL_BASE(ch, SKILL_WEAPON_LIGHT_THROWN), 
 			GET_SKILL_BASE(ch, SKILL_WEAPON_LIGHT_THROWN));
@@ -1466,7 +1466,7 @@ ACMD(do_skills) {
 			GET_SKILL_BASE(ch, SKILL_WEAPON_MEDIUM_EDGED), GET_SKILL_BASE(ch, SKILL_WEAPON_MEDIUM_BLUNT), GET_SKILL_BASE(ch, SKILL_WEAPON_SHORTBOW),
 			GET_SKILL_BASE(ch, SKILL_WEAPON_LONGBOW), GET_SKILL_BASE(ch, SKILL_WEAPON_HEAVY_CROSSBOW),
 			GET_SKILL_BASE(ch, SKILL_WEAPON_HEAVY_THROWN));
-		send_to_char(ch, "@r=- @cAlchemy@n:@Y%5d  @cEnchant@n:@Y%5d  @cRunecraft@n:@Y%5d@n @cForging@n:@Y%5d                  @r-=\r\n",
+		send_to_char(ch, "@r=- @cAlchemy@n:@Y%5d  @cEnchant@n:@Y%5d  @cRunecraft@n:@Y%5d@n     @cForging@n:@Y%5d                  @r-=\r\n",
 			GET_SKILL_BASE(ch, SKILL_WEAPON_HEAVY_EDGED), GET_SKILL_BASE(ch, SKILL_WEAPON_HEAVY_BLUNT),
 			GET_SKILL_BASE(ch, SKILL_WEAPON_POLEARM), GET_SKILL_BASE(ch, SKILL_WEAPON_BACKSTAB), GET_SKILL_BASE(ch, SKILL_WEAPON_LIGHT_THROWN), 
 			GET_SKILL_BASE(ch, SKILL_WEAPON_LIGHT_CROSSBOW));
