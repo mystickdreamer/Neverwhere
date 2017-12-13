425239b876c88ed9582be7f6e9590aeef842a6da
BuildTools <unconfigured@null.spigotmc.org>
2017-12-12 22:32:20 -0600

    making changed in act.information.c

--- act.informative.c
+++ act.informative.c
@@ -1047,7 +1047,7 @@ static void look_at_target(struct char_data *ch, char *arg, int cmread) {
 			if (ch != found_char) {
 				if (AFF_FLAGGED(ch, AFF_HIDE))
 					hidelooker = roll_resisted(ch, GET_SKILL_BASE(ch, SKILL_PERCEPTION), found_char, GET_SKILL_BASE(found_char, SKILL_STEALTH));
-				act("%d just a check",TRUE, hidelooker. 0, chm TO_VICT );
+				act("%d just a check",TRUE, hidelooker, 0, chm TO_VICT );
 				else
 					hidelooker = 0;
 				if (!hidelooker) {
