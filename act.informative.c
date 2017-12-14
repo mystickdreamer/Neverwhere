431b06194f1d7af829e0a7f595a2a142ca12e952
BuildTools <unconfigured@null.spigotmc.org>
2017-12-14 08:08:03 -0600

    random changes

--- act.informative.c
+++ act.informative.c
@@ -2005,10 +2005,10 @@ ACMD(do_who) {
 			} else {
 				num_can_see++;
 				if (CONFIG_ALLOW_MULTICLASS) {
-					send_to_char(ch, "@n[@c%s@n] @Y%s%s@n",
+					send_to_char(ch, "@n[@c%s@n] @Y%s @R%s@n",
 						RACE_ABBR(tch), GET_NAME(tch), GET_TITLE(tch));
 				} else {
-					send_to_char(ch, "[%s] %s%s%s",
+					send_to_char(ch, "[%s] %s @R%s%s@n",
 						RACE_ABBR(tch), GET_NAME(tch), *GET_TITLE(tch) ? " " : "", GET_TITLE(tch));
 				}
 
