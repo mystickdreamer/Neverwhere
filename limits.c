0d703e5e5b4436843106487b946df25df3ea3edb
BuildTools <unconfigured@null.spigotmc.org>
2017-12-14 22:40:23 -0600

    testing exp gain

--- limits.c
+++ limits.c
@@ -348,8 +348,7 @@ void learn_from_success(const struct char_data *ch, int i) {
 		if (GET_SKILL_BASE(ch, i) < CONFIG_LEVEL_CAP - 1 &&
 			GET_SKILL_XP(ch, i) >= level_exp(GET_SKILL_BASE(ch, i) + 1)) {
 			send_to_char(ch, "@rYour %s skill has gained a @ylevel@r.@n\r\n", spell_info[i].name);
-			val = GET_SKILL_BASE(ch, i) + 1;
-			SET_SKILL(ch, i, val);
+			SET_SKILL(ch, i, GET_SKILL_BASE(ch, i) + 1);
 		}
 	} else if (gain < 0) {
 		gain = MAX(-CONFIG_MAX_EXP_LOSS, gain); /* Cap max exp lost per death */
