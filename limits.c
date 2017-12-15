a2c2a2fbfb8f936c9f4c9a82eda15e6f46a65696
BuildTools <unconfigured@null.spigotmc.org>
2017-12-14 22:51:18 -0600

    testing exp gain

--- limits.c
+++ limits.c
@@ -352,7 +352,7 @@ void learn_from_success(const struct char_data *ch, int i) {
 		}
 	} else if (gain < 0) {
 		gain = MAX(-CONFIG_MAX_EXP_LOSS, gain); /* Cap max exp lost per death */
-		GET_SKILL_XP(ch, rand_number(1, 64)) += gain;
+		//GET_SKILL_XP(ch, rand_number(1, 64)) += gain;
 		if (GET_SKILL_XP(ch, i) < 0)
 			//GET_SKILL_XP(ch, i) = 0;
 	}
